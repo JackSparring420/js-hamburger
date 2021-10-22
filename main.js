49af53b7a68f5c8a616737d83b39677498c62b4b
JackSparring420 <emanuele.arconte94@gmail.com>
2021-10-22 17:16:36 +0200

    change solution

--- main.js
+++ main.js
@@ -1,21 +1,36 @@
 let list = document.querySelector(".header-right")
 let hamburger = document.querySelector(".hamburger-menu")
 let cross = document.querySelector(".close")
-const mediaQuery = window.matchMedia('(max-width: 1000px)')
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+// const mediaQuery = window.matchMedia('(max-width: 1000px)')
+// const mediaQueryB = window.matchMedia('(min-width: 1001px)')
 
 list.addEventListener("click",
     function(){
-        if(mediaQuery.matches){
-            hamburger.style.display = "block";
-        } else{
-            hamburger.style.display = "none";
-        }    
-    }
+        hamburgerMenu.classList.add('close');
+    }        
 )
 
 cross.addEventListener("click",
     function(){
-        hamburger.style.display = "none";
+        hamburgerMenu.classList.remove('close');
     }
 )
 
+
+
+// list.addEventListener("click",
+//     function(){
+//         if(mediaQuery.matches){
+//             hamburger.style.display = "block";
+//         } else{
+//             hamburger.style.display = "none";
+//         }    
+//     }
+// )
+
+// cross.addEventListener("click",
+//     function(){
+//         hamburger.style.display = "none";
+//     }
+// )
